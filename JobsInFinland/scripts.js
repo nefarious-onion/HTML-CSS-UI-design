981414f47837fcbf3fe19086be097389097eccae
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-03-19 21:49:57 +0200

    add parallax scrolling effect to hero image, add sticky navbar

---
+++ JobsInFinland/scripts.js
@@ -0,0 +1,23 @@
+const scrollPosition = {
+    x: 0,
+    y: 0
+}
+const parallaxBg = document.querySelector('.hero-image');
+
+const scrollLoop = () => {
+    //returns the number of pixels that document is currently scrolled on x and y axis
+    //scrollPosition.x = window.scrollX;
+    scrollPosition.y = window.scrollY;
+    //repositions background image in 3d -> 
+    //values change according to scrolled pixels a
+    //nd the multiplier sets the changed scroll speed of the background image
+    parallaxBg.style.transform = `translate3d(${scrollPosition.x}px, ${scrollPosition.y * -0.2}px, 0)`;
+
+    //inserts scrollLoop to browser's event loop 
+    window.requestAnimationFrame(scrollLoop);
+
+}
+
+//starts the loop after DOM has loaded
+//false prevents event bubbling
+window.addEventListener('DOMContentLoaded', scrollLoop, false);
